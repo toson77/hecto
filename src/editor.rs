3de112258eb60b356828f51d6d220d5829df6f97
Noriyuki <norifxsc@gmail.com>
2021-05-18 20:55:01 +0900

    separate reading from evaluating

---
+++ src/editor.rs
@@ -0,0 +1,39 @@
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        loop {
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
+        }
+    }
+    pub fn default() -> Self {
+        Self {}
+    }
+    fn process_keypress(&self) -> Result<(), std::io::Error> {
+        let process_key = read_key()?;
+        match process_key {
+            Key::Ctrl('c') => panic!("Program end"),
+            _ => (),
+        }
+        Ok(())
+    }
+}
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
+}
+fn die(e: std::io::Error) {
+    panic!(e);
+}
